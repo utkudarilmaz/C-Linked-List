a13df47a179f1243267d9234a3d878483fdb3684
utkudarilmaz <utkudarilmaz@gmail.com>
2017-10-26 18:07:50 +0300

    Add funct

--- Linked_List.c
+++ Linked_List.c
@@ -1,87 +1,140 @@
 #include "stdio.h"
 #include <stdlib.h>
+#include <time.h>
 
-typedef struct node{ // Node isminde tanımlanan düğüm yapısı
-    int data;
+typedef struct node{ // Node isminde tanimlanan dugum yapisi
+    double data;
     struct node *next;
 }Node;
 
 void add_Node(Node**,Node*);    // Dugum ekleme fonksiyonu
-void print_List(Node*);         // Dugumleri yazdırma fonksiyonu
+void print_List(Node*);         // Dugumleri yazdirma fonksiyonu
 void del_Node(Node**,Node*);    // Dugum silme fonksiyonu
-void search_Node(Node*);        // Bağlı listede arama yapan fonksiyon
+void search_Node(Node*);        // Bagli listede arama yapan fonksiyon
+void rand_five(Node**,Node*);
 
 int main(){
-    Node* head=NULL; // İlk düğümü gösteren pointer
+    srand (time(NULL));
+    Node* head=NULL; // Ilk dugumu gosteren pointer
     int secim_Menu,secim,sonsuz=1,i=0;
-    while(sonsuz){ // Menunun her ıslemden sonra gelmesini sağlayan döngü
-        printf("\n1: Tek eleman ekleme\n2: Birden fazla eleman ekleme\n3: Eleman silme\n4: Elemanlari yazdirma\n5: Eleman arama\n6: Cikis\nSecim: ");
-                scanf("%d",&secim_Menu );
+    for(;i<5;i++){
+        rand_five(&head,head);
+    }
+    while(sonsuz){ // Menunun her islemden sonra gelmesini saglayan dongu
+        printf("\n1: Tek eleman ekleme\n2: Eleman silme\n3: Elemanlari yazdirma\n4: Eleman arama\n0: Cikis\nSecim: ");
+        scanf("%d",&secim_Menu);
         switch (secim_Menu) { // Menu
-            case 1: // Bir dugum ekleme seçeneği
+            case 1: // Bir dugum ekleme secenegi
                 printf("\n");
                 add_Node(&head,head);
-                break;
-            case 2: // Birden fazla dugum ekleme seçeneği
-                printf("\nKac eleman girmek istiyorsunuz?\nSecim :");
-                scanf("%d",&secim);
-                while(i<secim){
-                    add_Node(&head,head);
-                    i++;
-                }
-                break;
-            case 3: // Dugum silme seceneği
-                del_Node(&head,head);
-                break;
-            case 4: // Baglı listeyi yazdıracak fonksiyon
                 print_List(head);
                 printf("\n");
                 break;
-            case 5: // Bağlı listede arama yapan fonksiyon
+            case 2: // Dugum silme secenegi
+                del_Node(&head,head);
+                print_List(head);
+                printf("\n");
+                break;
+            case 3: // Bagli listeyi yazdiracak fonksiyon
+                print_List(head);
+                printf("\n");
+                break;
+            case 4: // Bagli listede arama yapan fonksiyon
                 search_Node(head);
                 break;
-            case 6: // Çıkış
+            case 0: // Cikis
                 sonsuz=0;
                 break;
         }
         printf("----------------------------------------------------------------------------------------------\n");
     }
-
     return 0;
 }
 
-void add_Node(Node** head_null,Node* head){ // Dugum ekleme fonksiyonu
-    if(head==NULL){ // Baglı listenin ilk elemanının eklendiği kısım
+void rand_five(Node** head_null,Node* head){ // Dugume otomatik 5 dugum ekleyen ve her dugumun data degiskenine random deger ekleyen fonksiyon
+    if(head==NULL){ // Listeye ilk dugumu ekleyen kisim
         Node* new_Node=(Node*)malloc(sizeof(Node));
-        *head_null=new_Node;
-        printf("Deger: ");
-        scanf("%d",&(new_Node->data));
-        new_Node->next=NULL;
-        return;
-    }
-    else{ // İlk elemandan sonraki herhangi bir yere düğüm eklenilen kısım
+  		*head_null=new_Node;
+  		new_Node->data=rand()%100;
+  		new_Node->next=NULL;
+  		return;
+  	}
+    else{
         Node* temp;
         temp=head;
         while(temp!=NULL){
             if(temp->next!=NULL){
                 temp=temp->next;
-            }else break;
+            }
+            else break;
         }
         Node* new_Node=(Node*)malloc(sizeof(Node));
-        printf("Deger: ");
-        scanf("%d",&(new_Node->data));
+        new_Node->data=rand()%100;
         new_Node->next=NULL;
         temp->next=new_Node;
         return;
     }
 }
 
-void del_Node(Node** head_new,Node* head){ // Girilen sıradaki düğümü silecek fonksiyon
+void add_Node(Node** head_new,Node* head){ // Dugum ekleme fonksiyonu
+    int secim,sayac=0;
+    printf("Eklenilecek yer: ");
+    scanf("%d",&secim);
+    Node* sayim=head;
+    while(sayim!=NULL){
+        sayac++;
+        if(sayim->next!=NULL){
+            sayim=sayim->next;
+        }
+        else break;
+    }
+    if(secim==1){ // Bagli listenin basina ekleme
+        Node* new_Node=(Node*)malloc(sizeof(Node));
+        Node* temp=head;
+        *head_new=new_Node;
+        printf("Yeni elemanin degeri: ");
+        scanf("%lf",&(new_Node->data));
+        new_Node->next=temp;
+        return;
+    }
+    if(secim==sayac){ // Sona ekleme
+        Node* temp;
+        temp=head;
+        while(temp!=NULL){
+            if(temp->next!=NULL){
+                temp=temp->next;
+            }
+            else break;
+        }
+        Node* new_Node=(Node*)malloc(sizeof(Node));
+        printf("Yeni elemanin degeri: ");
+        scanf("%lf",&(new_Node->data));
+        new_Node->next=NULL;
+        temp->next=new_Node;
+        return;
+    }
+    else{ // Araya ekleme
+        Node* new_Node=(Node*)malloc(sizeof(Node));
+        Node* temp=head;
+        int i=1;
+        while(i<secim-1){
+      	     temp=temp->next;
+          	 i++;
+  		}
+        printf("Yeni eleman�n degeri: ");
+        scanf("%lf",&(new_Node->data));
+        new_Node->next=temp->next;
+        temp->next=new_Node;
+      	return;
+  	}
+}
+
+void del_Node(Node** head_new,Node* head){ // Girilen siradaki dugumu silecek fonksiyon
     int target;
     printf("\nKacinci elemani silmek istiyorsunuz?\nSecim :");
     scanf("%d",&target);
     Node* temp=head;
-    if(target==1){ // Eğer girilen değer ilk elemana tekabül ediyorsa bu kısım calısır
+    if(target==1){ // Eger girilen deger ilk elemana tekabul ediyorsa bu kisim calisir
         *head_new=temp->next;
         free(temp);
         return;
@@ -92,7 +145,7 @@ void del_Node(Node** head_new,Node* head){ // Girilen sıradaki düğümü silec
         i++;
     }
     Node* temp_ex;
-    if(temp->next->next==NULL){ // Eğer girilen değer son elemana tekabül ediyorsa burası çalışır
+    if(temp->next->next==NULL){ // Eger girilen deger son elemana tekabul ediyorsa burasi calisir
         temp_ex=temp->next;
         temp->next=NULL;
         free(temp_ex);
@@ -104,26 +157,27 @@ void del_Node(Node** head_new,Node* head){ // Girilen sıradaki düğümü silec
     printf("%d. Terim silindi\n",target);
 }
 
-void search_Node(Node* head){ // Bağlı listede arama yapan fonksiyon
+void search_Node(Node* head){ // Bagli listede arama yapan fonksiyon
     Node* temp=head;
-    int target,count=1;
+    float target;
+  	int count=1;
     printf("\nAranacak Deger: ");
-    scanf("%d",&target);
-    while(temp!=NULL){ // Dugum olduğu sürece arama yapılmasını sağlayan döngü
-        if(temp->data==target){ // Aranan değerin bulunma durumu
-            printf("Bulundu!\nAranan deger listenin %d. elemanı\n",count);
+    scanf("%f",&target);
+    while(temp!=NULL){ // Dugum oldugu sürece arama yapilmasini saglayan dongu
+        if(temp->data==target){ // Aranan degerin bulunma durumu
+            printf("Bulundu!\nAranan deger listenin %d. elemani\n",count);
             return;
         }
         temp=temp->next;
         count++;
     }
-    printf("Aranan deger listede bulunamadı!\n");
+    printf("Aranan deger listede bulunamad�!\n");
 }
 
-void print_List(Node* head){ // Bağlı listeyi yazdıran fonksiyon
+void print_List(Node* head){ // Bagli listeyi yazdiran fonksiyon
     Node* temp=head;
     do{
-        printf("%d\n",temp->data);
+        printf("%f\n",temp->data);
         temp=temp->next;
     }while(temp!=NULL);
 }
